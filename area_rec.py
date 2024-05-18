23571f1199b6152b3c9545c42c5ef8707a5452f1
JBBA01 <167823695+JBBA01@users.noreply.github.com>
2024-05-18 19:46:45 +0000

    Add files via upload

---
+++ area_rec.py
@@ -0,0 +1,10 @@
+def area_rectangulo(base, altura):
+    return base*altura
+
+base=5
+altura=10
+
+area= area_rectangulo(base, altura)
+print("el area del rectangulo es:",area)
+
+
---
+++ contar.py
@@ -0,0 +1,12 @@
+def contar_palabras(archivo):
+    with open(archivo,'r')as file:
+        contenido=file.read()
+        palabras=contenido.split()
+        num_palabras=len(palabras)
+        return num_palabras
+
+archivo="palabras.txt"
+total_palabras=contar_palabras(archivo)
+print("el total de palabras es: ", total_palabras)
+
+
